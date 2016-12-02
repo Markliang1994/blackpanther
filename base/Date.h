f4a6156976b1edcc76eff1e136c58fb036b2c723
liangshaocong <lsc19941028@gmail.com>
2016-12-02 20:02:59 +0800

    add timezone Date

---
+++ base/Date.h
@@ -0,0 +1,8 @@
+//
+// Created by mark on 16-12-2.
+//
+
+#ifndef BLACKPANTHER_DATE_H
+#define BLACKPANTHER_DATE_H
+
+#endif //BLACKPANTHER_DATE_H
--- base/TimeZone.h
+++ base/TimeZone.h
@@ -2,7 +2,34 @@
 // Created by mark on 16-12-1.
 //
 
-#ifndef BLACKPANTHER_TIMEZONE_H
-#define BLACKPANTHER_TIMEZONE_H
+#ifndef BLACKPANTHER_BASE_TIMEZONE_H
+#define BLACKPANTHER_BASE_TIMEZONE_H
 
+#include <blackpanther/base/copyable.h>
+#include <memory>
+#include <time.h>
+
+namespace blackpanther{
+    class TimeZone : copyable{
+    public:
+        explicit TimeZone(const char *zonefile);
+        TimeZone(int eastOfUtc, const char *tzname);
+        TimeZone(){ }
+
+        bool valid() const {
+            return static_cast<bool>(data_);
+        }
+
+        struct tm toLocalTime(time_t secondsSinceEpoch) const;
+        time_t fromLocalTime(const struct tm&)const;
+
+        static struct tm toUtcTime(time_t secondsSinceEpoch, bool yday = false);
+        static time_t fromUtcTime(const struct tm&);
+        static time_t fromUtcTime(int year, int month, int day, int hour, int minute, int seconds);
+
+        struct Data;
+    private:
+        std::shared_ptr<Data> data_;
+    };
+}
 #endif //BLACKPANTHER_TIMEZONE_H
