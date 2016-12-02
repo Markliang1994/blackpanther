b4d827af6ce14db60633969486a1ccc97c17c317
liangshaocong <lsc19941028@gmail.com>
2016-12-02 20:06:46 +0800

    fix TimeZone and Timestamp

--- base/Timestamp.h
+++ base/Timestamp.h
@@ -2,7 +2,16 @@
 // Created by mark on 16-12-1.
 //
 
-#ifndef BLACKPANTHER_TIMESTAMP_H
-#define BLACKPANTHER_TIMESTAMP_H
+#ifndef BLACKPANTHER_BASE_TIMESTAMP_H
+#define BLACKPANTHER_BASE_TIMESTAMP_H
 
+namespace blackpanther{
+    class Timestamp{
+    public:
+        static Timestamp now();
+        static Timestamp invalid(){
+            return Timestamp();
+        }
+    };
+}
 #endif //BLACKPANTHER_TIMESTAMP_H
