d4c5050de1a34307bd937818717df8af7a72d0db
liangshaocong <lsc19941028@gmail.com>
2016-12-04 15:03:55 +0800

    fix Timestamp

--- base/Timestamp.h
+++ base/Timestamp.h
@@ -5,13 +5,65 @@
 #ifndef BLACKPANTHER_BASE_TIMESTAMP_H
 #define BLACKPANTHER_BASE_TIMESTAMP_H
 
+#include <blackpanther/base/copyable.h>
+
+#include <stdint.h>
+#include <time.h>
+#include <string>
+
 namespace blackpanther{
-    class Timestamp{
+    class Timestamp : public blackpanther::copyable{
     public:
+        Timestamp()
+                :microSecondsSinceEpoch_(0){ }
+
+        explicit Timestamp(int64_t microSecondsSinceEpochArg)
+                : microSecondsSinceEpoch_(microSecondsSinceEpochArg)
+        {
+        }
+
+        void swap(Timestamp &that){
+            std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
+        }
+        std::string toString()const;
+        std::string toFormattedString(bool showMicroSeconds = true) const;
+
+        bool valid() const { return microSecondsSinceEpoch_ > 0; }
+
+        int64_t microSecondsSinceEpoch() const { return microSecondsSinceEpoch_; }
+        time_t secondsSinceEpoch() const{
+            return static_cast<time_t>(microSecondsSinceEpoch_/kMicroSecondsPerSecond);
+        }
+
         static Timestamp now();
         static Timestamp invalid(){
             return Timestamp();
         }
+
+        static Timestamp fromUnixTime(time_t t, int microSeconds){
+            return Timestamp(static_cast<int64_t>(t)*kMicroSecondsPerSecond + microSeconds);
+        }
+        static const int kMicroSecondsPerSecond = 1000*1000;
+    private:
+        int64_t microSecondsSinceEpoch_;
     };
+
+    inline bool operator <(Timestamp lhs, Timestamp rhs){
+        return lhs.microSecondsSinceEpoch() < rhs.microSecondsSinceEpoch();
+    }
+
+    inline bool operator ==(Timestamp lhs, Timestamp rhs){
+        return lhs.microSecondsSinceEpoch() == rhs.microSecondsSinceEpoch();
+    }
+
+    inline double timeDifference(Timestamp high, Timestamp low){
+        int64_t diff = high.microSecondsSinceEpoch() - low.microSecondsSinceEpoch();
+        return static_cast<double>(diff);
+    }
+
+    inline Timestamp addTime(Timestamp timestamp, double seconds){
+        int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
+        return Timestamp(timestamp.microSecondsSinceEpoch() + delta);
+    }
 }
 #endif //BLACKPANTHER_TIMESTAMP_H
