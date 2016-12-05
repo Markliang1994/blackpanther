b26fbd41c78a6338852309d5f292bf6a300f1c73
liangshaocong <lsc19941028@gmail.com>
2016-12-05 17:00:02 +0800

    test getYearMonthDay()

--- base/test/Date_unittest.cpp
+++ base/test/Date_unittest.cpp
@@ -4,6 +4,11 @@
 
 #include <blackpanther/base/Date.h>
 
+using namespace blackpanther;
+
 int main(void){
+    Date d(2016,2,3);
+    printf("julianDayNumber of 2000/01/01 is :%d\n",d.julianDayNumber());
+    d.toIsoString();
     return 0;
 }
