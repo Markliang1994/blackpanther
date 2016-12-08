5706a693e1c9bc5bbd8c61f69293e57ed1b40197
liangshaocong <lsc19941028@gmail.com>
2016-12-08 14:08:02 +0800

    add Channel

--- base/WeakCallback.h
+++ base/WeakCallback.h
@@ -10,16 +10,32 @@
 
 namespace blackpanther{
     template <typename T, typename ...args>
-    class WeakCallBack{
+    class WeakCallback{
     public:
-        WeakCallBack(const std::weak_ptr<T> &object,
+        WeakCallback(const std::weak_ptr<T> &object,
                      const std::function<void (T*, args...)> &function)
                 : object_(object), function_(function){
+        }
+
+        void operator()(args&& ...arg_list) const{
+            std::shared_ptr<T> ptr(object_.lock());
+            if(ptr)
+                function_(ptr.get());
         }
 
     private:
         std::weak_ptr<T> object_;
         std::function<void(T*)> function_;
     };
+    template <typename T, typename ...args>
+    WeakCallback<T> makeWeakCallback(const std::shared_ptr<T> &object, void (T::*function)(args...)){
+        return WeakCallback<T>(object, function);
+    }
+
+    template <typename T, typename ...args>
+    WeakCallback<T> makeWeakCallback(const std::shared_ptr<T> &object, void(T::*function)(args...)const){
+        return WeakCallback<T>(object, function);
+    }
 }
+
 #endif //BLACKPANTHER_WEAKCALLBACK_H
---
+++ net/Channel.h
@@ -0,0 +1,91 @@
+//
+// Created by mark on 16-12-8.
+//
+
+#ifndef BLACKPANTHER_NET_CHANNEL_H
+#define BLACKPANTHER_NET_CHANNEL_H
+
+#include <blackpanther/base/Timestamp.h>
+#include <blackpanther/base/noncopyable.h>
+
+#include <functional>
+#include <memory>
+
+namespace blackpanther{
+    namespace net{
+        class EventLoop;
+
+        class Channel : noncopyable{
+        public:
+            typedef std::function<void()> EventCallback;
+            typedef std::function<void(Timestamp)> ReadEventCallback;
+
+            Channel(EventLoop *loop, int fd);
+            ~Channel();
+
+            void handleEvent(Timestamp receiveTime);
+
+            void setReadCallback(const ReadEventCallback &cb){ readCallback_ = cb; }
+            void setWriteCallback(const EventCallback& cb) {  writeCallback_ = cb; }
+            void setCloseCallback(const EventCallback& cb) { closeCallback_ = cb; }
+            void setErrorCallback(const EventCallback& cb) { errorCallback_ = cb; }
+
+            // rvalue
+            void setReadCallback(ReadEventCallback &&cb) { readCallback_ = std::move(cb); }
+            void setWriteCallback(EventCallback &&cb) { writeCallback_ = std::move(cb); }
+            void setCloseCallback(EventCallback &&cb) { closeCallback_ = std::move(cb); }
+            void setErrorCallback(EventCallback &&cb) { errorCallback_ = std::move(cb); }
+
+            void tie(const std::shared_ptr<void> &obj);
+
+            int fd() const { return fd_; }
+            int events() const { return events_; }
+            void setRevents(int revt) { revents_ = revt; }
+            bool isNoneEvents() const { return events_ == kNoneEvent; }
+
+            void enableReading() { events_ |= kReadEvent; update(); }
+            void disableReading() { events_ &= ~kReadEvent; update(); }
+            void enableWritting() { events_ |= kWriteEvent; update(); }
+            void disableWritting() { events_ &= ~kWriteEvent; update(); }
+            void disableAll() { events_ = kNoneEvent; update(); }
+            bool isWritting() const { return events_ & kWriteEvent; }
+            bool isReading() const { return events_ & kReadEvent; }
+
+            int index() { return index_; }
+            void setIndex(int idx) { index_ = idx; }
+
+            std::string reventsToString() const;
+            std::string eventsToString() const;
+
+            EventLoop* ownerLoop() { return loop_; }
+            void remove();
+
+        private:
+            static std::string eventsToString(int fd, int ev);
+
+            void update();
+            void handleEventWithGuard(Timestamp receiveTime);
+
+            static const int kNoneEvent;
+            static const int kReadEvent;
+            static const int kWriteEvent;
+
+            EventLoop *loop_;
+            const int fd_;
+            int events_;
+            int revents_;
+            int index_;
+            bool logHup_;
+
+            std::weak_ptr<void> tie_;
+            bool tied_;
+            bool eventHandling_;
+            bool addedToLoop_;
+            ReadEventCallback readCallback_;
+            EventCallback writeCallback_;
+            EventCallback closeCallback_;
+            EventCallback errorCallback_;
+        };
+    }
+}
+#endif //BLACKPANTHER_CHANNEL_H
