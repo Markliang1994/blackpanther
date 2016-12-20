37ef28362942f63aa794737e48602bba43de9cf0
liang shaocong <liangshaocong1994@gmail.com>
2016-12-20 14:29:50 +0800

    Acceptor

--- net/Acceptor.cpp
+++ net/Acceptor.cpp
@@ -2,3 +2,64 @@
 // Created by mark on 16-12-20.
 //
 
+#include <blackpanther/base/Logging.h>
+#include <blackpanther/net/Acceptor.h>
+#include <blackpanther/net/EventLoop.h>
+#include <blackpanther/net/InetAddress.h>
+#include <blackpanther/net/SocketsOps.h>
+
+#include <errno.h>
+#include <fcntl.h>
+
+using namespace blackpanther;
+using namespace blackpanther::net;
+
+Acceptor::Acceptor(EventLoop *loop, const InetAddress &listenAddr, bool reuseport)
+        : loop_(loop),
+          acceptSocket_(sockets::createNonblockingOrDie(listenAddr.family())),
+          acceptChannel_(loop_, acceptSocket_.fd()),
+          listenning_(false),
+          idleFd_(::open("/dev/null", O_RDONLY | O_CLOEXEC)){
+    assert(idleFd_ > 0);
+    acceptSocket_.setReuseAddr(true);
+    acceptSocket_.setReusePort(reuseport);
+    acceptSocket_.bindAddress(listenAddr);
+    acceptChannel_.setReadCallback(std::bind(&Acceptor::handleRead, this));
+}
+
+Acceptor::~Acceptor() {
+    acceptChannel_.disableAll();
+    acceptChannel_.remove();
+    ::close(idleFd_);
+}
+
+void Acceptor::listen() {
+    loop_->assertInLoopThread();
+    listenning_ = true;
+    acceptSocket_.listen();
+    acceptChannel_.enableReading();
+}
+
+void Acceptor::handleRead(){
+    loop_->assertInLoopThread();
+    InetAddress peeraddr;
+    // FIXME loop until more
+    int connfd = acceptSocket_.accept(&peeraddr);
+    if(connfd >= 0){
+        if(newConnectionCallback_){
+            newConnectionCallback_(connfd, peeraddr);
+        }
+        else{
+            sockets::close(connfd);
+        }
+    }
+    else{
+        LOG_SYSERR << "in Acceptor::handleRead";
+        if(errno == EMFILE){
+            ::close(idleFd_);
+            idleFd_ = ::accept(acceptSocket_.fd(), nullptr, nullptr);
+            ::close(idleFd_);
+            idleFd_ = ::open("/dev/null", O_RDONLY | O_CLOEXEC);
+        }
+    }
+}
--- net/Acceptor.h
+++ net/Acceptor.h
@@ -2,7 +2,45 @@
 // Created by mark on 16-12-20.
 //
 
-#ifndef BLACKPANTHER_ACCEPTOR_H
-#define BLACKPANTHER_ACCEPTOR_H
+#ifndef BLACKPANTHER_NET_ACCEPTOR_H
+#define BLACKPANTHER_NET_ACCEPTOR_H
 
+#include <blackpanther/net/Channel.h>
+#include <blackpanther/net/Socket.h>
+
+#include <blackpanther/base/noncopyable.h>
+
+#include <functional>
+
+namespace blackpanther{
+    namespace net{
+        class EventLoop;
+        class InetAddress;
+
+        class Acceptor : public blackpanther::noncopyable{
+        public:
+            typedef std::function<void(int sockfd, const InetAddress&)> NewConnectionCallback;
+
+            Acceptor(EventLoop *loop, const InetAddress &listenAddr, bool reuseport);
+            ~Acceptor();
+
+            void setNewConnectionCallback(const NewConnectionCallback &cb){
+                newConnectionCallback_ = cb;
+            }
+
+            bool listenning(){ return listenning_; }
+            void listen();
+
+        private:
+            void handleRead();
+
+            EventLoop *loop_;
+            Socket acceptSocket_;
+            Channel acceptChannel_;
+            NewConnectionCallback newConnectionCallback_;
+            bool listenning_;
+            int idleFd_;
+        };
+    }
+}
 #endif //BLACKPANTHER_ACCEPTOR_H
