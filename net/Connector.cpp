549ef6c69e0bd559f1f22c27324729bd1a28abbb
liang shaocong <liangshaocong1994@gmail.com>
2016-12-20 10:55:12 +0800

    connector

---
+++ net/Connector.cpp
@@ -0,0 +1,182 @@
+//
+// Created by mark on 16-12-20.
+//
+#include <blackpanther/net/Connector.h>
+#include <blackpanther/net/Channel.h>
+#include <blackpanther/net/EventLoop.h>
+#include <blackpanther/net/SocketsOps.h>
+
+#include <blackpanther/base/Logging.h>
+
+#include <errno.h>
+
+using namespace blackpanther;
+using namespace blackpanther::net;
+
+const int Connector::kMaxRetryDelayMs;
+
+Connector::Connector(EventLoop *loop, const InetAddress &serverAddr)
+        : loop_(loop),
+          serverAddr_(serverAddr),
+          state_(kDisconnected),
+          retryDelayMs_(kInitRetryDelayMs)
+{
+    LOG_DEBUG << "ctor[" << this << "]";
+}
+
+Connector::~Connector(){
+    LOG_DEBUG <<"dtor[" << this << "]";
+    assert(!channel_);
+}
+
+void Connector::start(){
+    connect_ = true;
+    loop_->runInLoop(std::bind(&Connector::startInLoop, this));
+}
+
+void Connector::startInLoop(){
+    loop_->assertInLoopThread();
+    assert(state_ == kDisconnected);
+    if(connect_ == true){
+        connect();
+    }
+    else{
+        LOG_DEBUG << "do not connect";
+    }
+}
+
+void Connector::stop(){
+    connect_ = false;
+    loop_->queueInLoop(std::bind(&Connector::stopInLoop, this));
+}
+
+void Connector::stopInLoop(){
+    loop_->assertInLoopThread();
+    if(state_ == kConnecting){
+        setState(kDisconnected);
+        int sockfd = removeAndResetChannel();
+        retry(sockfd);
+    }
+}
+
+void Connector::connect(){
+    int sockfd = sockets::createNonblockingOrDie(serverAddr_.family());
+    int ret = sockets::connect(sockfd, serverAddr_.getSockAddr());
+    int savedErrno = (ret == 0) ? 0 : errno;
+    switch(savedErrno){
+        case 0:
+        case EINPROGRESS:
+        case EINTR:
+        case EISCONN:
+            connecting(sockfd);
+            break;
+        case EAGAIN:
+        case EADDRINUSE:
+        case EADDRNOTAVAIL:
+        case ECONNREFUSED:
+        case ENETUNREACH:
+            retry(sockfd);
+            break;
+
+        case EACCES:
+        case EPERM:
+        case EAFNOSUPPORT:
+        case EALREADY:
+        case EBADF:
+        case EFAULT:
+        case ENOTSOCK:
+            LOG_SYSERR << "connector error in Connector::startInLoop" << savedErrno;
+            sockets::close(sockfd);
+            break;
+
+        default:
+            LOG_SYSERR << "Unexpected error in Connector::startInLoop" << savedErrno;
+            sockets::close(sockfd);
+            break;
+    }
+}
+
+void Connector::restart(){
+    loop_->assertInLoopThread();
+    setState(kDisconnected);
+    retryDelayMs_ = kInitRetryDelayMs;
+    connect_ = true;
+    startInLoop();
+}
+
+void Connector::connecting(int sockfd) {
+    setState(kConnecting);
+    assert(!channel_);
+
+    channel_.reset(new Channel(loop_, sockfd));
+    channel_->setWriteCallback(std::bind(&Connector::handleWrite, this));
+    channel_->setErrorCallback(std::bind(&Connector::handleError, this));
+
+    channel_->enableWritting();
+}
+
+int Connector::removeAndResetChannel() {
+    channel_->disableAll();
+    channel_->remove();
+    int sockfd = channel_->fd();
+
+    loop_->queueInLoop(std::bind(&Connector::resetChannel, this));
+    return sockfd;
+}
+
+void Connector::resetChannel() {
+    channel_.reset();
+}
+
+void Connector::handleWrite() {
+    LOG_TRACE << "Connector::handleWrite " << state_;
+
+    if(state_ == kConnecting){
+        int sockfd = removeAndResetChannel();
+        int err = sockets::getSocketError(sockfd);
+        if(err){
+            LOG_WARN << "Connector::handleWrite - SO_ERROR = " << err << " " << strerror_tl(err);
+            retry(sockfd);
+        }
+        else if(sockets::isSelfConnect(sockfd)){
+            LOG_WARN << "Connector::handleWrite - TCP Self connect";
+            retry(sockfd);
+        }
+        else{
+            setState(kConnected);
+            if(connect_){
+                newConnectionCallback_(sockfd);
+            }
+            else{
+                sockets::close(sockfd);
+            }
+        }
+    }
+    else{
+        assert(state_ == kDisconnected);
+    }
+}
+
+void Connector::handleError(){
+    LOG_ERROR << "Connector::handleError state = " << state_;
+    if(state_ == kConnecting){
+        int sockfd = removeAndResetChannel();
+        int err = sockets::getSocketError(sockfd);
+        LOG_TRACE << "SO_ERROR = " << err << " " << strerror_tl(err);
+        retry(sockfd);
+    }
+}
+
+void Connector::retry(int sockfd) {
+    sockets::close(sockfd);
+    setState(kDisconnected);
+    if(connect_){
+        LOG_INFO << "Connector::retry - Retry connecting to " << serverAddr_.toIpPort()
+                                                              << " in " << retryDelayMs_ << " milliseconds. ";
+        loop_->runAfter(retryDelayMs_/1000.0, std::bind(&Connector::startInLoop, shared_from_this()));
+        retryDelayMs_ = std::min(retryDelayMs_*2, kMaxRetryDelayMs);
+    }
+    else{
+        LOG_DEBUG << "do not connect";
+    }
+}
---
+++ net/Connector.h
@@ -0,0 +1,63 @@
+//
+// Created by mark on 16-12-20.
+//
+
+#ifndef BLACKPANTHER_NET_CONNECTOR_H
+#define BLACKPANTHER_NET_CONNECTOR_H
+
+#include <blackpanther/net/InetAddress.h>
+#include <blackpanther/base/noncopyable.h>
+
+#include <memory>
+#include <functional>
+
+namespace blackpanther{
+    namespace net{
+        class Channel;
+        class EventLoop;
+
+        class Connector : public std::enable_shared_from_this<Connector>, blackpanther::noncopyable{
+        public:
+            typedef std::function<void(int sockfd)> NewConnectionCallback;
+
+            Connector(EventLoop *loop, const InetAddress &serverAddr);
+            ~Connector();
+
+            void setNewConnectionCallback(const NewConnectionCallback &cb){
+                newConnectionCallback_ = cb;
+            }
+
+            void start();
+            void restart();
+            void stop();
+
+            const InetAddress& serverInetAddress() const { return serverAddr_; }
+
+        private:
+            enum States { kDisconnected, kConnecting, kConnected };
+
+            static const int kMaxRetryDelayMs = 30*1000;
+            static const int kInitRetryDelayMs = 500;
+
+            void setState(States s){ state_ = s; }
+            void startInLoop();
+            void stopInLoop();
+            void connect();
+            void connecting(int sockfd);
+            void handleWrite();
+            void handleError();
+            void retry(int sockfd);
+            int removeAndResetChannel();
+            void resetChannel();
+
+            EventLoop *loop_;
+            InetAddress serverAddr_;
+            bool connect_;
+            States state_;
+            std::unique_ptr<Channel> channel_;
+            NewConnectionCallback newConnectionCallback_;
+            int retryDelayMs_;
+        };
+    }
+}
+#endif //BLACKPANTHER_CONNECTOR_H
