cd35a05bd1d0d3ef2e3a3d063a7d5a393529e52a
mark <liangshaocong1994.gmail.com>
2017-01-02 06:28:11 +0000

    new gcc atomic interface

--- base/Atomic.h
+++ base/Atomic.h
@@ -16,29 +16,11 @@ namespace blackpanther{
         class AtomicInteger : boost::noncopyable{
         public:
             T get(){
-#ifdef __GNUC__
-                #if __GNUC__ > 4 || \
-                    (__GNUC__ == 4 && __GNUC_MINOR__ >= 7 && __GNUC_PATCHLEVEL___ >= 0)
                         return __atomic_load_n(&value_, __ATOMIC_SEQ_CST);
-                #else
-                     return __sync_val_compare_and_swap(&value_, 0, 0);
-                #endif
-#else
-                return __sync_val_compare_and_swap(&value_, 0, 0);
-#endif
             }
 
             T getAndAdd(T x){
-#ifdef __GNUC__
-                #if __GNUC__ > 4 || \
-                (__GNUC__ == 4 && __GNUC_MINOR__ >= 7 && __GNUC_PATCHLEVEL___ >= 0)
                     return __atomic_fetch_add(&value_, x, __ATOMIC_SEQ_CST);
-                #else
-                    return __sync_fetch_and_add(&value_, x);
-                #endif
-#else
-                return __sync_fetch_and_add(&value_, x);
-#endif
             }
 
             T addAndGet(T x){
@@ -68,17 +50,7 @@ namespace blackpanther{
             }
 
             T getAndSet(T newValue){
-#ifdef __GNUC__
-                #if __GNUC__ > 4 || \
-                    (__GNUC__ == 4 && __GNUC_MINOR__ >= 7 && __GNUC_PATCHLEVEL___ >= 0)
-                     __atomic_store_n(&value_, newValue, __ATOMIC_SEQ_CST);
-                     return newValue;
-                #else
-                    return __sync_lock_test_and_set(&value_, newValue);
-                #endif
-#else
-                return __sync_lock_test_and_set(&value_, newValue);
-#endif
+                return __atomic_exchange_n(&value_, newValue, __ATOMIC_SEQ_CST);
             }
 
         private:
